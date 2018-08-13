fc14de959dabf22ca997a23d848ed29edc98a6bb
Hans Olofsson <hans.olofsson@me.com>
2018-08-13 15:10:07 +0200

    Lagt till page-not-found komponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {HemComponent} from './hem/hem.component';
 import {OmComponent} from './om/om.component';
 import {ArbetsgivareComponent} from './arbetsgivare/arbetsgivare.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/hem', pathMatch: 'full'},
   {path: 'hem', component: HemComponent},
   {path: 'arbetsgivare', component: ArbetsgivareComponent},
   {path: 'om', component: OmComponent},
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HemComponent } from './hem/hem.component';
 import { OmComponent } from './om/om.component';
 import { ArbetsgivareModule } from './arbetsgivare/arbetsgivare.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HemComponent,
-    OmComponent
+    OmComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
