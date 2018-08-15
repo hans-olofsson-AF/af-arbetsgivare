a8f20dec6d71deedd15c473b5fa1c2aebbaf0ea2
Hans Olofsson <hans.olofsson@me.com>
2018-08-15 16:14:01 +0200

    Services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HemComponent } from './hem/hem.component';
 import { OmComponent } from './om/om.component';
 import { ArbetsgivareModule } from './arbetsgivare/arbetsgivare.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import {ArbetsgivareService} from './services/arbetsgivare.service';
+import {StateService} from './services/state.service';
 
 @NgModule({
   declarations: [
--- src/app/services/arbetsgivare.service.ts
+++ src/app/services/arbetsgivare.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import {HttpClient} from '@angular/common/http';
+import {HttpErrorResponse} from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ArbetsgivareService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getOrganisation(orgnr: string) {
+    const url = 'assets/organisation/' + orgnr + '.json';
+    console.log('OrganisationService.getOrganisation - url: ' + url);
+    return this.http.get(url);
+  }
+
+  public getArbetsplats(kundnr: number) {
+    const url = 'assets/arbetsplats/' + kundnr + '.json';
+    console.log('OrganisationService.getArbetsplats - url: ' + url);
+    return this.http.get(url);
+  }
 }
