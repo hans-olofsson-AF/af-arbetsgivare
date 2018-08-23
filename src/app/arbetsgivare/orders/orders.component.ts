dd9a3aecb8ea758355cc732f353802d68c865ccc
Hans Olofsson <hans.olofsson@me.com>
2018-08-23 13:09:07 +0200

    Lade till content.

--- src/app/arbetsgivare/orders/orders.component.ts
+++ src/app/arbetsgivare/orders/orders.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StateService } from '../../services/state.service';
+
+import { Arbetsplats } from '../../model/arbetsplats';
+import { OrderMini } from '../../model/orderMini';
+
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
+  public arbetsplats: Arbetsplats = new Arbetsplats();
+  private buttonTextExpand = 'Visa ordrar';
+  private buttonTextCollapse = 'Dölj ordrar';
+  public isCollapsed = true;
+  public buttonText = this.buttonTextExpand;
 
-  constructor() { }
+  constructor(private state: StateService) {}
 
   ngOnInit() {
+    this.state.arbetsplats.subscribe(res => (this.arbetsplats = res));
   }
 
+  onClick(ordersMini: OrderMini) {
+    console.log('klickad order: ' + ordersMini.id);
+    // this.state.loadArbetsplats(arbetsplatsMini.kundnr);
+  }
+
+  toggle() {
+    if (this.isCollapsed) {
+      this.buttonText = this.buttonTextCollapse;
+      this.isCollapsed = false;
+    } else {
+      this.buttonText = this.buttonTextExpand;
+      this.isCollapsed = true;
+    }
+  }
 }
